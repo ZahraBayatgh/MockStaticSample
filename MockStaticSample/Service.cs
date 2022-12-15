571da265f88de4ece5696149b4f9cac3e8757824
Zahra Bayat <58395585+ZahraBayatgh@users.noreply.github.com>
2022-12-15 16:21:42 +0330

    Add project files.

---
+++ MockStaticSample/Service.cs
@@ -0,0 +1,15 @@
+﻿namespace MockStaticSample
+{
+    public class Service
+    {
+        public bool Check(int min, int max)
+        {
+           var result= Generator.GenerateCode(min,max);
+           
+            if (result>max/2)
+                return true;
+            
+            return false;
+        }
+    }
+}
---
+++ MockStaticSampleTests/ServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using MockStaticSample;
+using Xunit;
+
+namespace MockStaticSampleTests
+{
+    public class ServiceTests
+    {
+        [Fact]
+        public void CheckTest()
+        {
+            //Arrange
+            var service = new Service();
+            
+            var min = 10;
+            var max = 100;
+
+            Generator.GenerateCode = (x, y) => { return 60; };
+
+            //Act
+            var result = service.Check(min, max);
+           
+            //Assert
+            Assert.True(result);
+        }
+    }
+}

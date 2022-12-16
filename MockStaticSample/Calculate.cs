d457772777ec711d3e06370cffc83be82dad0009
Zahra Bayat <58395585+ZahraBayatgh@users.noreply.github.com>
2022-12-16 11:44:27 +0330

    Code refactor

--- MockStaticSample/Calculate.cs
+++ MockStaticSample/Calculate.cs
@@ -2,15 +2,6 @@
 {
     public static class Generator
     {
-        //public static Func<int, int, int> Add = (a, b) =>
-        //{
-        //    return a + b;
-        //};
-        //public static int GenerateCode()
-        //{
-        //    return new Random().Next(20, 100);
-
-        //}
         public static Func<int, int, int> GenerateCode = (a,b) =>
         {
             return new Random().Next(a, b);
